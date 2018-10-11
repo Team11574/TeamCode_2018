5ce9e5afff89ca41c726f775ffe77836c47585ca
Jeremy Cole <jeremy@jcole.us>
2018-10-11 15:00:20 -0700

    Added TeleOpTest; first program for 2018!

---
+++ src/main/java/us/ftcteam11574/teamcode2018/TeleOpTest.java
@@ -0,0 +1,41 @@
+package us.ftcteam11574.teamcode2018;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+@SuppressWarnings({"unused"})
+public class TeleOpTest extends OpMode {
+    private DcMotor mL, mR, mW;
+    private Servo sH;
+
+    @Override
+    public void init() {
+        mL = hardwareMap.dcMotor.get("mL");
+        mL.setDirection(DcMotorSimple.Direction.REVERSE);
+        mR = hardwareMap.dcMotor.get("mR");
+        mR.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        mW = hardwareMap.dcMotor.get("mW");
+        mW.setDirection(DcMotorSimple.Direction.FORWARD);
+        mW.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        sH = hardwareMap.servo.get("sH");
+        sH.setDirection(Servo.Direction.REVERSE);
+        sH.setPosition(0.0);
+    }
+
+    @Override
+    public void loop() {
+        mL.setPower(-gamepad1.left_stick_y);
+        mR.setPower(-gamepad1.right_stick_y);
+        mW.setPower(gamepad1.right_stick_x);
+        sH.setPosition(gamepad1.right_trigger);
+    }
+}
+
+
+
