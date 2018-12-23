5d04162f4224943ee581769be89e1905fbc7d084
SonicSophia <31019181+SonicSophia@users.noreply.github.com>
2018-12-23 13:41:26 -0800

    Create TestDistances.java

---
+++ src/main/java/us/ftcteam11574/teamcode2018/TestDistances.java
@@ -0,0 +1,34 @@
+package us.ftcteam11574.teamcode2018;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+public class TestDistances extends OpMode {
+    private Rev2mDistanceSensor drr;
+    private Rev2mDistanceSensor drf;
+    private Rev2mDistanceSensor df;
+
+    @Override
+    public void init() {
+        drr = hardwareMap.get(Rev2mDistanceSensor.class , "drr");
+        drf = hardwareMap.get(Rev2mDistanceSensor.class , "drf");
+        df =  hardwareMap.get(Rev2mDistanceSensor.class , "df");
+
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("drr" , drr.getDistance(DistanceUnit.MM));
+        telemetry.addData("drf", drf.getDistance(DistanceUnit.MM));
+        telemetry.addData("df" , df.getDistance(DistanceUnit.MM));
+        telemetry.update();
+
+
+
+    }
+}
