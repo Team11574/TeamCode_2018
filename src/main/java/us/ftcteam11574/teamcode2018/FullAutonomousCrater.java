e2dbc788e064853f8d7570fa21871540d712aa34
Sara-Sommer <Tallyho944@gmail.com>
2019-01-21 16:57:58 -0800

    Successful Full Autonomous!!!!! (:

--- src/main/java/us/ftcteam11574/teamcode2018/FullAutonomousCrater.java
+++ src/main/java/us/ftcteam11574/teamcode2018/FullAutonomousCrater.java
@@ -12,47 +12,35 @@ public class FullAutonomousCrater extends GenericAutonomous {
 
         robotDetachRobotFromLander();
 
-        driveMoveToRelativePosition(300, 300, 0.5);
+        driveMoveToRelativePosition(240, 240, 0.5);
 
-        if (mineralPosition == GoldMineralLocator.MineralPosition.RIGHT) {
-            driveMoveToAngle(25, 0.5);
-        } else if (mineralPosition == GoldMineralLocator.MineralPosition.LEFT) {
-            driveMoveToAngle(-25, 0.5);
+        if (mineralPosition == GoldMineralLocator.MineralPosition.RIGHT
+                || mineralPosition == GoldMineralLocator.MineralPosition.LEFT) {
+            double reverseAngle = 1.0;
+            if (mineralPosition == GoldMineralLocator.MineralPosition.LEFT)
+                reverseAngle = -1.0;
+            driveMoveToAngle(reverseAngle * 18, 0.5);
+            driveMoveToRelativePosition(500, 500, 0.5);
+            driveMoveToRelativePosition(-500, -500, 0.5);
+        } else {
+            driveMoveToRelativePosition(500, 500, 0.5);
+            driveMoveToRelativePosition(-500, -500, 0.5);
         }
 
-        driveMoveToRelativePosition(500, 500, 0.5);
-        driveMoveToRelativePosition(-400, -400, 0.5);
-        driveMoveToAngle(-65, 0.5);
+        driveMoveToAngle(-50, 0.5);
         waitTime(250);
-        driveToDiagonalDistance(150, 0.75);
+        driveMoveToRelativePosition(1000, 1000, 0.75);
         driveMoveToAngle(getAngle() - 10, 0.5);
-        driveToDistanceUsingSideSensor(70, 0.5);
+        driveMoveToRelativePosition(100, 100, 0.5);
         driveMoveToAngle(getAngle() -10, 0.5);
-        driveToDistanceParallelToWall(550, 70, 1.0);
+        driveMoveToRelativePosition(100, 100, 0.5);
+        driveMoveToAngle(-130, 0.25);
+        waitTime(250);
+        //driveToDiagonalDistance(150, 0.75);
+        //driveToDistanceUsingSideSensor(70, 0.5);
+        driveMoveToRelativePosition(750, 750, 0.75);
+
         winchMoveToZero();
-        double distanceFromWall = getDistanceFromFront();
-        driveDistanceParallelToWallUsingEncoders(-(1700 - distanceFromWall), 70, 1.0);
-
-
-       /* if (mineralPosition == GoldMineralLocator.MineralPosition.UNKNOWN ||
-                mineralPosition == GoldMineralLocator.MineralPosition.CENTER) {
-            driveMoveToRelativePosition(1100, 1100,
-                    Constants.DRIVE_SPEED_TO_PARK);
-        } else if (mineralPosition == GoldMineralLocator.MineralPosition.LEFT ||
-                mineralPosition==GoldMineralLocator.MineralPosition.RIGHT) {
-            double m = 1.0;
-            if (mineralPosition == GoldMineralLocator.MineralPosition.RIGHT)
-                m = -1.0;
-            driveMoveToRelativePosition(180, 180, Constants.DRIVE_SPEED_TO_PARK);
-            driveMoveToRelativePosition(m * -170, m * 170, Constants.DRIVE_SPEED_TO_PARK);
-            driveMoveToRelativePosition(850, 850, Constants.DRIVE_SPEED_TO_PARK);
-            driveMoveToRelativePosition(m * 250, m * -250, Constants.DRIVE_SPEED_TO_PARK);
-            driveMoveToRelativePosition(450, 450, Constants.DRIVE_SPEED_TO_PARK);
-        }
-        */
-
-        // winch down below horizontal to drop the team marker
-        winchMoveToZero();
-
-    }
+        driveMoveToRelativePosition(-1500, -1500, 0.5);
+          }
 }
